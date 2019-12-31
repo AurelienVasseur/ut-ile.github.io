95a8c923ecf565226d909157b2540172af5c6443
hicham.arahhou <hichamarahhou@yahoo.fr>
2020-01-01 00:53:35 +0100

    added firebase config properties to production environment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDZUzpY0qYJcLlrznHGN6ZysolWL5GSb-g",
+    authDomain: "ut-ile.firebaseapp.com",
+    databaseURL: "https://ut-ile.firebaseio.com",
+    projectId: "ut-ile",
+    storageBucket: "ut-ile.appspot.com",
+    messagingSenderId: "1009692095278",
+    appId: "1:1009692095278:web:cf3da2d61b1599e73340bf",
+    measurementId: "G-1NQEMZ9TG0"
+  }
 };
